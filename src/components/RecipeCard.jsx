dace3418db80d4e6a353c3643b5211ccc0d05aa2
Pradipti7 <pradiptipokharel7@gmail.com>
2026-07-04 18:46:42 +0545

    add: Initial commit for RecipeCard

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,17 @@
+function RecipeCard({recipe}){
+    return(
+        <div className="col-md-4 md-4">
+            <div className="card shadow h-100">
+                <img src={recipe.images} className="card-img-top" style={{height:"220px", objectFit:"cover"}} />
+                <div className="card-body">
+                    <h4>{recipe.title}</h4>
+                    <p>{recipe.description}</p>
+                    <button className="btn btn-sucess">
+                        View Recipe
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default RecipeCard;
