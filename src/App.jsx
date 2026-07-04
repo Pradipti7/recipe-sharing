455a1b7cb50bf0318c2c7e64e7eb3f52a91f99ab
Pradipti7 <pradiptipokharel7@gmail.com>
2026-07-05 05:44:22 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-function App (){
-  retrurn(
-    
-  )
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  retrurn();
 }
