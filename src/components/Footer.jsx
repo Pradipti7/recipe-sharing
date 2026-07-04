dfe6e7e048fc079a9aeae8db866e1c9fd26e523e
Pradipti7 <pradiptipokharel7@gmail.com>
2026-07-04 18:40:24 +0545

    add: initial commit for footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer className="bg-dark text-white text-center py-3 mt-5"> 
+        <p className="mb-0"> 
+            © 2026 RecipeHub | Made by Pradipti with React & Tailwind CSS
+        </p>
+        </footer>
+    )
+}export default Footer; 
