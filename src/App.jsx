6853df7328af085f247faad4d85840ce271ee7ef
Pradipti7 <pradiptipokharel7@gmail.com>
2026-07-05 05:45:59 +0545

    update: added features on App pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import AddRecipe from "./pages/AddRecipe";
 
-function App (){
-  retrurn(
-    
-  )
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  ((
+    <BrowserRouter>
+      <Navbar />
+      <Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/add" element={<AddRecipe />} />
+      </Route>
+    </BrowserRouter>
+  ),
+    retrurn());
 }
