49d4cdc3b77741ecffc0ad0f38fe41cd168c12f8
Pradipti7 <pradiptipokharel7@gmail.com>
2026-07-05 06:03:30 +0545

    add: added new components on apps

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App (){
-  retrurn(
-    
-  )
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import AddRecipe from "./pages/AddRecipe";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/recipes" element={<Recipes />} />
+
+        <Route path="/add" element={<AddRecipe />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  );
 }
+
+export default App;
